30b64593db58f0134c35f664144af57567628110
nikitajuneja1 <52567770+nikitajuneja1@users.noreply.github.com>
2020-02-21 00:33:38 +0530

    Add files via upload

---
+++ hashcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+long int B, L, D, i;
+cin>>B;
+cin>>L;
+cin>>D;
+long int S[B];
+for(i=0; i<S.length(); i++){
+    cin>>S[i];
+}
+long int Nj[L], Tj[L], Mj[L];
+for(i=0; i<L; i++){
+cin>>Nj[i];
+cin>>Tj[i];
+cin>>Mj[i];
+}
+}
